fb101a3f7b95a1313af48959d8b0bda2d9b58400
gordon <gordon@hypernumbers.com>
2023-01-01 17:28:30 +0000

    use levels for documentation properly

--- make_book.sh
+++ make_book.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-~/Dev/literatecodereader/literate_compiler/literate_compiler -i ./the_manual -o ./docs
+~/Dev/literatecodereader/literate_compiler/literate_compiler -i ./the_manual -o ./docs --type 0
