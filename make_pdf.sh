cdacb12ab0097c025199a453a880b390a7cc521b
gordon <gordon@hypernumbers.com>
2023-04-09 14:28:40 +0100

    make the manual prperly

--- make_pdf.sh
+++ make_pdf.sh
@@ -1,4 +1,23 @@
 #!/bin/bash
 wkhtmltopdf --javascript-delay 400 \
 http://gordonguthrie.github.io/TheManual \
+https://gordonguthrie.github.io/TheManual/chapter1/introduction.html \
+https://gordonguthrie.github.io/TheManual/chapter1/installing_supercollider.html \
+https://gordonguthrie.github.io/TheManual/chapter1/first_beep_in_supercollider.html \
+https://gordonguthrie.github.io/TheManual/chapter1/first_synth_in_sonic_pi.html \
+https://gordonguthrie.github.io/TheManual/chapter1/setting_up.html \
+https://gordonguthrie.github.io/TheManual/chapter2/the_synths.html \
+https://gordonguthrie.github.io/TheManual/chapter2/overtone_synths.html \
+https://gordonguthrie.github.io/TheManual/chapter2/supercollider_synths.html \
+https://gordonguthrie.github.io/TheManual/chapter3/deep_dive.html \
+https://gordonguthrie.github.io/TheManual/chapter3/investigating_sonic_pi_and_error_messages.html \
+https://gordonguthrie.github.io/TheManual/chapter3/loggin_loggin_loggin.html \
+https://gordonguthrie.github.io/TheManual/chapter3/how_built_in_synthdefs_are_loaded.html \
+https://gordonguthrie.github.io/TheManual/chapter3/how_sonic_pi_plays_built_in_synths.html \
+https://gordonguthrie.github.io/TheManual/chapter4/how_built_in_synths_behave.html \
+https://gordonguthrie.github.io/TheManual/chapter5/ugens_channels_mixing_and_panning.html \
+https://gordonguthrie.github.io/TheManual/chapter5/second_synth.html \
+https://gordonguthrie.github.io/TheManual/chapter5/moar_ugen_stuff.html \
+https://gordonguthrie.github.io/TheManual/chapter5/funky_stuff_part_uno_variables.html \
+https://gordonguthrie.github.io/TheManual/chapter6/whats_next.html \
 the_manual.pdf
