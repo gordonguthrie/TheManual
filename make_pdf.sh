c3d7ec05f56ae5afcd128d90c584ec8a1f0526ff
gordon <gordon@hypernumbers.com>
2023-04-15 16:06:59 +0100

    fix up the pdf of the manual

--- make_pdf.sh
+++ make_pdf.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-wkhtmltopdf --javascript-delay 400 \
+wkhtmltopdf --javascript-delay 800 \
 http://gordonguthrie.github.io/TheManual \
 https://gordonguthrie.github.io/TheManual/chapter1/introduction.html \
 https://gordonguthrie.github.io/TheManual/chapter1/installing_supercollider.html \
@@ -13,8 +13,9 @@ https://gordonguthrie.github.io/TheManual/chapter3/deep_dive.html \
 https://gordonguthrie.github.io/TheManual/chapter3/investigating_sonic_pi_and_error_messages.html \
 https://gordonguthrie.github.io/TheManual/chapter3/loggin_loggin_loggin.html \
 https://gordonguthrie.github.io/TheManual/chapter3/how_built_in_synthdefs_are_loaded.html \
-https://gordonguthrie.github.io/TheManual/chapter3/how_sonic_pi_plays_built_in_synths.html \
-https://gordonguthrie.github.io/TheManual/chapter4/how_built_in_synths_behave.html \
+https://gordonguthrie.github.io/TheManual/chapter3/how_sonic_pi_plays_synths.html \
+https://gordonguthrie.github.io/TheManual/chapter4/how_built_in_synths_are_defined_in_Sonic_Pi.html \
+https://gordonguthrie.github.io/TheManual/chapter4/the_synth_beep.html \
 https://gordonguthrie.github.io/TheManual/chapter5/ugens_channels_mixing_and_panning.html \
 https://gordonguthrie.github.io/TheManual/chapter5/second_synth.html \
 https://gordonguthrie.github.io/TheManual/chapter5/moar_ugen_stuff.html \
