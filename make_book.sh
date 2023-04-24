841f1153ff3977b0c248cf1c9841d82dcc874051
gordon <gordon@hypernumbers.com>
2023-04-24 20:08:15 +0100

    add the 2nd synth

--- make_book.sh
+++ make_book.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-~/Dev/literatecodereader/literate_compiler/literate_compiler -i ./the_manual -o ./docs --type 0
+~/Dev/literatecodereader/literate_compiler/literate_compiler -i ./the_manual -o ./docs --type 2
